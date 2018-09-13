481430d0112181a4c499dd71be5a87c035226fc6
Matthew Edwards <m.edwards3@ncl.ac.uk>
2018-09-13 14:44:52 -0600

    univariate diagnostics now the same as univariate eda

--- scripts/diag/diag_univar.R
+++ scripts/diag/diag_univar.R
@@ -1,20 +1,15 @@
 ##########################
-# Diagnostics.           #
+# Univariate Diagnostics.#
 ##########################
 
 # Command line arguments
 # =======================
 # - 1) Variable name.
-# - 2) Ensemble size.
-# - 3) AR order.
-# - 4) MA order.
-# - 5) Taper.
-# - 6) Simulated ensemble size.
 args = commandArgs(TRUE)
 
-# Libraries.
-# ========================
-package_names = c("tidyverse", "abind", "plyr")
+# Load libraries.
+# =======================
+package_names = c("tidyverse", "parallel", "Hmisc", "RNetCDF", "plyr", "gridExtra")
 lapply(package_names, library, character.only = TRUE)
 
 # Set Working Directory (bash scripts).
@@ -22,65 +17,55 @@ lapply(package_names, library, character.only = TRUE)
 setwd("/Users/matthewedwards/Sync/Projects/data-compression")
 
 # Source functions.
-# ========================
-source("scripts/diag/diag_fun.R")
+# =======================
+source("scripts/temp/temp_fun.R")
+source("scripts/eda/eda_fun.R")
 
-# Load files.
-# ========================
+# Load data.
+# =======================
+load(file = paste0("data/", args[1], "/Y.r", args[2], ".p", args[3], ".q", args[4], ".t", args[5], ".s", args[6], ".R"))
+load(file = paste0("data/", args[1], "/year.R"))
 load(file = paste0("data/", args[1], "/lon.R"))
 load(file = paste0("data/", args[1], "/lat.R"))
-load(file = paste0("data/", args[1], "/year.R"))
-D.sim = get(load(file = paste0("data/", args[1], "/D.r", args[2], ".p", args[3], ".q", args[4], ".t", args[5], ".s", args[6], ".R")))
-D     = get(load(file = paste0("data/", args[1], "/D.R"))) 
 
-# Test sets.
-# ========================
-D = D[(as.numeric(args[2]) + 1):dim(D)[1], , , ]
+# Variable Statistics.
+# =======================
+var_w = weight_array(Y[1, , , ], lat)
+cl = makeCluster(detectCores() - 1)
+var_q = parApply(cl, Y, 1, wtd.quantile, weights = var_w)
+var_m = parApply(cl, Y, 1, wtd.mean, weights = var_w)
+stopCluster(cl)
 
-# Longitude and latitude indices.
-# ========================
-lon.ind = c(1, 33, 65, 97, 129, 161, 193, 225, 257)
-lat.ind = 1:(dim(D)[4] - 1)
+# Variable data frame.
+# =======================
+print(round(data.frame(Means = c(mean(var_m), rowMeans(var_q)),
+                       SDs   = c(sd(var_m), apply(var_q, 1, sd))), 2))
 
-# Annomoly arrays.
-# ========================
-D = abind(D[, , lon.ind, lat.ind], D[, , lon.ind + 1, lat.ind], 
-          D[, , lon.ind, lat.ind + 1], D[, , lon.ind + 1, lat.ind + 1], rev.along = 0)
-D.sim = abind(D.sim[, , lon.ind, lat.ind], D.sim[, , lon.ind + 1, lat.ind], 
-              D.sim[, , lon.ind, lat.ind + 1], D.sim[, , lon.ind + 1, lat.ind + 1], rev.along = 0)
+# Temporal and latitudinal statistics.
+# =======================
+temp_w = weight_array(Y[1, 1, , ], lat)
+cl = makeCluster(detectCores() - 1)
+temp_m = parApply(cl, Y, 1:2, wtd.mean, weights = temp_w)
+lat_m  = parApply(cl, Y, c(1, 4), mean)
+stopCluster(cl)
 
-# Longitudes and latitudes.
-# ========================
-lon = lon[lon.ind]
-lat = lat[lat.ind]
-
-# Spatio-temporal covariances.
-# ========================
-TempC = aaply(D[, , , , 1], c(1, 3, 4), temp_acf, .progress = "text")
-TempC.sim = aaply(D.sim[, , , , 1], c(1, 3, 4), temp_acf, .progress = "text")
-LonC = aaply(D[, , , ,c(1, 2)], c(1, 3, 4), temp_ccf, .progress = "text")
-LonC.sim = aaply(D.sim[, , , ,c(1, 2)], c(1, 3, 4), temp_ccf, .progress = "text")
-LatC = aaply(D[, , , ,c(1, 3)], c(1, 3, 4), temp_ccf, .progress = "text")
-LatC.sim  = aaply(D.sim[, , , ,c(1, 3)], c(1, 3, 4), temp_ccf, .progress = "text")
-
-# Temporal-covariance.
-# ========================
-png(paste0("plots/", args[1], "/temp_cov.r", args[2], ".p", args[3], ".q", args[4], ".t", args[5], ".s", args[6], ".png"), width = 800, height = 800)
-diag_plot_grid(TempC[, , , 2], TempC.sim[, , , 2], lat, lon, "Temporal-correlation (lag 1)", args[1])
+# Temporal and latitudinal statistics plot.
+# =======================
+png(paste0("plots/", args, "/temp_lat.r5.p3.q0.t10.s5.png"), width = 900, height = 900)
+grid.arrange(ensemble_plot(year, temp_m, "Year", paste("Global Mean", args), seq(2010, 2100, 10)), 
+             ensemble_plot(lat, lat_m, "Latitude", paste("Latitudinal Mean", args), seq(-90, 90, 30)))
 dev.off()
 
-# Longitudinal-covariance.
+# Spatial Statistics.
 # ========================
-png(paste0("plots/", args[1], "/lon_cov.r", args[2], ".p", args[3], ".q", args[4], ".t", args[5], ".s", args[6], ".png"), width = 800, height = 800)
-diag_plot_grid(LonC[, , , 1], LonC.sim[, , , 1], lat, lon, "Longitudinal-correlation (lag 1)", args[1])
-dev.off()
+cl = makeCluster(detectCores() - 1)
+spat_p = apply(Y[1, , , ], 2:3, spat_pars)
+stopCluster(cl)
 
-# Latitudinal-covariance.
+# Spatial NetCDF File.
 # ========================
-png(paste0("plots/", args[1], "/lat_cov.r", args[2], ".p", args[3], ".q", args[4], ".t", args[5], ".s", args[6], ".png"), width = 800, height = 800)
-diag_plot_grid(LatC[, , , 1], LatC.sim[, , , 1], lat, lon, "Latitudinal-correlation (lag 1)", args[1])
-dev.off()
+map2(alply(spat_p, 1), c("intercept.r5.p3.q0.t10.s5", "trend.r5.p3.q0.t10.s5", "std.r5.p3.q0.t10.s5", "auto_cov.r5.p3.q0.t10.s5"), save_ncdf, lon = lon, lat = lat, args = args)
 
 # Clear workspace.
-# ========================
+# =======================
 rm(list = ls())
